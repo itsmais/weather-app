081aba46e4bf1db6ef883313845bde528e32d985
Mais <59611735+itsmais@users.noreply.github.com>
2020-08-23 23:38:44 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# weather-app
-Returns the weather in your local area.
+# flight-status
+Created on the National Aviation Day of the US, Flight Status returns the status of a flight after providing its icao24 number.
+I couldn't publish it on GitHub pages because the API's free plan doesn't support HTTPS.
+### Screen of the Website
+<p float="left">
+  <img src="screenshots/screenshot.PNG" width="700" />
+</p>
---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+function convertToCelsius(x) {
+    x = parseFloat(x);
+    return Math.floor(x - 273.15);
+}
+let weatherResult = document.getElementById("weather-result");
+let weahterIcon = document.getElementById("icon");
+
+function getWeather() {
+    weatherResult.innerHTML = "";
+    navigator.geolocation.getCurrentPosition(function (position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+
+        if (navigator.geolocation) {
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+            };
+
+            let url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=6abdc02a8c5609bade1268cceec08c45";
+            console.log(url);
+            fetch(url, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    var jsonObj = JSON.parse(result);
+                    // console.log(jsonObj);
+                    let weatherDescription = jsonObj["weather"][0]["description"];
+                    let temp = convertToCelsius(jsonObj["main"]["temp"]);
+                    let locationName = jsonObj["name"];
+                    weatherResult.innerHTML +=
+                        "it is " + weatherDescription +
+                        " </br> in " + locationName +
+                        "</br>" + temp + "\xB0 C";
+                    weahterIcon.src = "https://openweathermap.org/img/wn/" + jsonObj["weather"][0]["icon"] + "@2x.png";
+
+                })
+                .catch(error => {
+                    //console.log('error', error)
+                });
+
+        }
+    });
+}
